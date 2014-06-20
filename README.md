78f926641db25a174902fd89fd88452072a0c789
Allan Jiang <origami789@gmail.com>
2014-06-20 14:53:35 -0400

    first commit, possibly buggy

--- README.md
+++ README.md
@@ -1,4 +1,101 @@
 epistasis-simulation
 ====================
+Creating simulated data to test statistical techniques to detect gene-gene interactions.
 
-creating simulated data to test statistical techniques to detect gene-gene interactions.
+### Why this?
+The idea is to fabricate a dataset where you know the underlying gene-gene interactions so you can test which of the statistical techniques is best able to pick up the interactions you want it to.
+
+### How do I run it?
+The main file is `make_data.R`. The code is commented. Run the whole file.
+
+### Analysis:
+Our analysis includes 3 variants, 3 phenotypes, and 1000 subjects.
+Our interaction network has the following structure:
+```
+     [,1] [,2] [,3]
+[1,]  0.0  0.9  0.0
+[2,]  0.4  0.0  0.8
+[3,]  0.0  0.0  0.0
+```
+This means that delta_12 = .9, delta_21 = .4, and delta_23 = .8
+
+We then do a transformation
+```R
+int_adj <- int_adj + int_adj %^% 2 + int_adj %^% 3 - (diag(n_v) * (int_adj %^% 2))
+```
+to turn this underlying interaction network into one that includes indirect influences. We do this because we need a network of overall variant to variant influences in order to create our dataset.
+The result is:
+```
+      [,1]  [,2]  [,3]
+[1,] 0.000 1.224 0.720
+[2,] 0.544 0.000 1.088
+[3,] 0.000 0.000 0.000
+```
+
+We also create our main effects, which are as follows:
+```
+     [,1] [,2] [,3]
+[1,]  1.0  1.0  1.0
+[2,]  3.0  3.5  4.0
+[3,]  3.5  4.0  3.0
+[4,]  4.0  3.0  3.5
+```
+
+From this, we create our beta matrix, and separately create a genotype distribution in order to create our whole fabricated dataset.
+The objects included in the fabricated dataset should include `design` and `phenos`.
+
+Now is time to "reverse solve" for the interaction values using our statistical techniques.
+####cape
+The first test is to see how well the current methodology fares.
+After performing our "reverse solve" algorithm by doing pairwise regressions on TWO phenotypes at a time, and comparing them to our interaction adjacency matrix from which we created the data, we see that our results (as expected) are not bad.
+```
+> print(signif(delta_cape2, digits=3))
+     v1 v2       d21  d12
+[1,]  1  2  5.44e-01 1.22
+[2,]  1  3 -4.22e-15 0.72
+[3,]  2  3  1.78e-15 1.09
+> int_adj
+      [,1]  [,2]  [,3]
+[1,] 0.000 1.224 0.720
+[2,] 0.544 0.000 1.088
+[3,] 0.000 0.000 0.000
+```
+However, when running a "reverse solve" by regressing deltas against ALL phenotypes, we get the following result:
+```
+> print(signif(B_sep, digits=3))
+       [,1] [,2]   [,3]
+[1,]  0.000 4.85  1.340
+[2,] -3.630 0.00 -0.584
+[3,] -0.815 1.23  0.000
+```
+Oddly, we get a strong negative signal at [2,1], and weaker negative signals at [3,1], and [2,3].
+
+The interpretation of this is still unclear to me.
+
+### separation of overall influences
+In this method, we take all the overall beta outputs given to us by cape, and try to infer the direct influences from those overal influences.
+```
+> print(signif(A_sep, digits=3))
+       [,1]  [,2]   [,3]
+[1,]  0.897 0.191  0.250
+[2,] -0.246 0.875  0.257
+[3,]  0.218 0.310 -0.113
+```
+Again, this makes little sense to me. For some reason, the [1,1] effect is stronger than the [1,2] effect. The same negative effects remain. This is bad...
+
+### directly search for direct influences using optimization method
+In this method, we do the regression on the equation beta_12 = beta1 (d21 + d23 * d31) + beta2 (d12 + d13 * d32) in the 3 variable case. The code does generalize to more variables.
+Our output in this case is
+```
+> print(signif(B_dir, digits=3))
+       [,1]  [,2]  [,3]
+[1,]  0.000 4.780 0.222
+[2,] -3.680 0.000 0.235
+[3,]  0.199 0.276 0.000
+```
+This one still has that strong negative effect on [2,1]. Looks like it never went away.
+
+# Conclusion
+So far, our ability to detect the interactions using any of the 3 methods are looking pretty bad.
+Of course, there are many different ways to turn our underlying interaction matrix into a fabricated dataset, and I hypothesize that the manner by which we create `int_adj` will make a large impact on how well our methods perform. In addition, the statistical techniques themselves can be tweaked.
+
---
+++ make_data.R
@@ -0,0 +1,128 @@
+##################################################
+# Making a dataset for analysis: genotype and phenotype information
+# --> specify the following:
+#   --> number of genes and phenotypes to create
+#   --> genotypic distribution...
+#   --> underlying genotype network
+#   --> underlying main effects
+#   --> model to use to get phenotype from genotype
+#   --> noise level/noise type
+########
+# test performance of my two methodologies and cape(?)
+##################################################
+# for now, we  do 3 genes and 3 phenotypes
+# there are 14 possible gene networks.
+# we are doing this one:
+#  A <--> B
+#        /
+#       v
+#      C
+##################################################
+library("MASS") #get ginv() for generalized inverse
+library("expm") #matrix exponentiation
+setwd("/Users/mingya/JAX/SSP/bin/simulation/v1.1")
+#set of helper fns that we use throughout
+source("helper_fn.R")
+#all the minimization code
+source("direct_minimize.R")
+
+#############################################
+### Define basic parameters:
+### number of variants, number of phenotypes,
+### number of individuals in the dataset
+#############################################
+n_v <- 3 #num variant
+n_p <- 3 #num pheno
+n_i <- 1000 #num indiv
+
+#############################################
+### Define underlying genetic network: 
+### set up the underlying variant interactions 
+### that you want to test. Observe the format
+### for interaction input. It will be converted
+### into an adjacency matrix for you.
+#############################################
+AtoB <- c(1,2,0.9)
+BtoA <- c(2,1,0.4)
+BtoC <- c(2,3,0.8)
+interactions <- rbind(AtoB, BtoA, BtoC)
+colnames(interactions) <- c("v1", "v2", "weight")
+int_adj <- get.var.adj.matrix(interactions, n_v) 
+#^^ this is a delta matrix, NOT a m_ij matrix
+
+#### the theoretical step!!! ####
+### manipulating the adjacency matrix to create "overall effects"
+#int_adj <- ginv(diag(n_v) - int_adj) - diag(n_v)
+int_adj <- int_adj + int_adj %^% 2 + int_adj %^% 3 - (diag(n_v) * (int_adj %^% 2))
+
+#############################################
+### Define the main effects:
+### place these into a matrix. The columns
+### are phenotypes, the rows are variants
+### remember to put a leading row of 1's!
+#############################################
+# model to get phenotype values from genotype (and beta values)
+main_effect_betas <- matrix(c(1, 3, 3.5, 4, 1, 3.5, 4, 3, 1, 4, 3, 3.5), 
+                            nrow = n_p + 1, ncol = n_v)
+
+### Calculating the full matrix of deltas,
+### including main effect AND combined effect coefficients
+betas <- get.beta.values(main_effect_betas, int_adj)
+
+#############################################
+### Create genotype distribution
+### Assign a probability that a variant will
+### exist at a locus.
+#############################################
+# create genotypic distribution
+var_prob <- 0.50 #30% chance variant exists at locus.
+design <- create.genotype.population(n_v, var_prob, n_i)
+
+#############################################
+### Create phenotype distribution
+### Creates the dataset given the genotype 
+### distribution, the underlying interactions,
+### and the main effects.
+#############################################
+phenos <- design %*% betas
+
+# TODO: incorporate noise of varying degrees...
+
+########## "Solving" for our interaction values ##########
+#cape method:
+#############################################
+### Using cape's methodology
+### B_cape --> get coefficients of pairwise regression
+###     matrix form
+### delta_cape2 --> calculate deltas wrt 2 phenos
+###     melted matrix form
+### delta_cape --> calculate deltas wrt all phenos
+###     melted matrix form
+#############################################
+B_cape <- ginv(design) %*% phenos #get betas
+delta_cape2 <- get.cape.deltas2(B_cape, n_v)
+delta_cape <- get.cape.deltas(B_cape, n_v)
+print(signif(delta_cape, digits=3))
+
+#separation method (get overall influnce, separate into direct and indirect)
+#############################################
+### Using the separation of overall influence method
+### B_sep --> reshaped version of the delta_cape 
+### melted matrix
+### A_sep --> matrix of inferred direct influence
+#############################################
+B_sep <- get.sep.adj.matrix(delta_cape)
+A_sep <- diag(n_v) - ginv(diag(n_v) + B_sep)
+print(signif(A_sep, digits=3))
+
+
+#direct search method (directly regress against many values of "delta")
+#degree 2
+#############################################
+### Using the direct search of direct influence method
+### result --> output of optim() function
+### B_dir --> reshaping result into adjacency matrix
+#############################################
+result <- bfgs_opt()
+B_dir <- melt(reshape.with.diag(result$par,n_v))
+
